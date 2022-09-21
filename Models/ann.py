af65174a652da5fe9fbd35b7d0c361bd941007ae
matanini <matanini@gmail.com>
2022-09-21 11:02:23 +0300

    .

--- Models/ann.py
+++ Models/ann.py
@@ -5,21 +5,38 @@ import numpy as np
 import pandas as pd
 
 class ANN:
-    def __init__(self, layout:tuple, activation:str):
+    def __init__(self, layout:tuple, activation:str, size):
         self.network=[]
         self.activation = activation
-        self._init_layers(layout)
+        self.layout = layout
+        self._init_layers(self.layout, size)
+        
 
-    def _init_layers(self ,layout):
+    def _init_layers(self ,layout, size):
         for layer in layout:
             inner_layer = []
-            for _ in range(layer):
-                inner_layer.append(Perceptron([],[],0,self.activation))
+            for i in range(layer):
+                inner_layer.append(Perceptron())
+                inner_layer[i].set_weights(self._generate_init_weights(size))
             self.network.append(inner_layer)
 
-    def _generate_init_weights(self):
-        pass
+    def _generate_init_weights(self, size):
+        random_weights = np.random.rand(size)
+        random_weights = [x if np.random.randint(3)!=2 else -x for x in random_weights]
+        return random_weights
 
         
-    def fit(X : pd.DataFrame, y:pd.Series):
-        pass
+    def fit(self, X : pd.DataFrame, y:pd.Series):
+
+        self._init_layers(self.layout, X.shape[1])
+        for index, row in X.iterrows():
+            pass
+
+    def __str__(self):
+        text = ''
+        for layer in self.network:
+            for p in layer:
+                text += str(p) + ' '
+            text += '\n'
+        return text
+    
--- models/perceptron.py
+++ models/perceptron.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 class Perceptron:
-    def __init__(self, inputs, weights, bias, activation_func: str, learning_const=0.1):
+    def __init__(self, inputs=None, weights=None, bias=None, activation_func: str = "sign", learning_const=0.1):
 
-        if len(inputs) != len(weights):
+        if input is not None and weights is not None and len(inputs) != len(weights):
             raise Exception("Vectors must have same size")
 
         self.inputs = np.array(inputs)
@@ -12,19 +12,21 @@ class Perceptron:
         self.bias = bias
         self.activation_func = activation_func
         self.learning_const = learning_const
+        self.output = None
         self._activation_fucntion = {
             "sign": self._sign,
             "sigmoid": self._sigmoid,
             "tanh": self._tanh,
             "relu": self._relu,
         }
-        self._activation_fucntion_der = {
-            "sign": self._sign_der,
-            "sigmoid": self._sigmoid_der,
-            "tanh": self._tanh_der,
-            "relu": self._relu_der,
+        self._activation_fucntion_deriv = {
+            "sign": self._sign_deriv,
+            "sigmoid": self._sigmoid_deriv,
+            "tanh": self._tanh_deriv,
+            "relu": self._relu_deriv,
         }
 
+    def predict(self):
         self.output = self.feedforward()
 
     def feedforward(self):
@@ -43,39 +45,43 @@ class Perceptron:
             print("----")"""
 
             prev_delta_weights = delta_weight.copy()
-            der = self._activation_fucntion_der.get(self.activation_func)
-            delta_weight = delta_weight + self.learning_const * self.inputs * der(_class)
+            deriv = self._activation_fucntion_deriv.get(self.activation_func)
+            delta_weight = delta_weight + self.learning_const * self.inputs * deriv(_class)
             self.weights = self.weights + delta_weight
-            self.output = self.feedforward()
+            self.predict()
 
     def _sign(self, x):
         return 1 if x > 0 else 0
 
-    def _sign_der(self, _class):
+    def _sign_deriv(self, _class):
         return _class - self.output
 
     def _sigmoid(self, x):
         sig = 1 / (1 + np.exp(-x))
         return 1 if sig >= 0.5 else 0
 
-    def _sigmoid_der(self, _class):
+    def _sigmoid_deriv(self, _class):
         return (_class - self.output) * self.output * (1 - self.output)
 
     def _tanh(self, x):
         sig = (np.exp(2 * x) - 1) / (np.exp(2 * x) + 1)
         return 1 if sig >= 0 else 0
 
-    def _tanh_der(self, _class):
+    def _tanh_deriv(self, _class):
         return _class - self.output
 
     def _relu(self, x):
         return max(0, x)
 
-    def _relu_der(self, _class):
+    def _relu_deriv(self, _class):
         return _class - self.output
 
     def set_weights(self, weights):
         self.weights = np.array(weights)
 
+    def set_inputs(self, inputs):
+        self.inputs = np.array(inputs)
+
     def __str__(self):
-        return f"Perceptron: {self.output}"
+        o = self.output if self.output != None else 'na'
+        return f"Perceptron: {o}"
