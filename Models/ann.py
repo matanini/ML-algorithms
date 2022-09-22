f79252bbc8b2b4751c930b922b2bc43a53e0feec
matanini <matanini@gmail.com>
2022-09-22 21:15:05 +0300

    update

--- Models/ann.py
+++ Models/ann.py
@@ -4,39 +4,62 @@ from Models.perceptron import Perceptron
 import numpy as np
 import pandas as pd
 
+
 class ANN:
-    def __init__(self, layout:tuple, activation:str, size):
-        self.network=[]
+    def __init__(self, layout: tuple, activation: str):
+        self.network = []
         self.activation = activation
         self.layout = layout
-        self._init_layers(self.layout, size)
-        
+        # self._init_layers(self.layout, size)
 
-    def _init_layers(self ,layout, size):
-        for layer in layout:
+    def _init_layers(self, f_vsize, output_nodes):
+        self.output_layer = np.array([] * output_nodes)
+        self.vector_size_array = [f_vsize] + list(self.layout) + [output_nodes]
+        print(self.vector_size_array)
+
+        for i in range(1, len(self.vector_size_array)):
             inner_layer = []
-            for i in range(layer):
-                inner_layer.append(Perceptron())
-                inner_layer[i].set_weights(self._generate_init_weights(size))
+            for _ in range(self.vector_size_array[i]):
+                inner_layer.append(Perceptron(weights=self._generate_init_weights(self.vector_size_array[i - 1]), activation_func=self.activation, output_layer=True))
             self.network.append(inner_layer)
 
     def _generate_init_weights(self, size):
         random_weights = np.random.rand(size)
-        random_weights = [x if np.random.randint(3)!=2 else -x for x in random_weights]
+        random_weights = [x if np.random.randint(3) != 2 else -x for x in random_weights]
         return random_weights
 
-        
-    def fit(self, X : pd.DataFrame, y:pd.Series):
+    def fit(self, X: pd.DataFrame, y: pd.Series):
 
-        self._init_layers(self.layout, X.shape[1])
+        self._init_layers(X.shape[1], len(y.unique()))
+        
         for index, row in X.iterrows():
-            pass
+            
+            # Feed forward
+            f_v = row.tolist()
+            for layer in self.network:
+                pred = []
+                for p in layer:
+                    p.set_inputs(f_v)
+                    pred.append(p.fit(y.loc[index]))
+                f_v = pred
+
+            # Backpropagation
+            output_delta_array = []
+            for o in pred:
+                output_delta_array.append(o * (1-o) * (y.loc[index] - o))
+            print(f"output delta array:{output_delta_array}")
+            for 
+
+
 
     def __str__(self):
-        text = ''
-        for layer in self.network:
+        text = f"ANN of dim {tuple(self.vector_size_array[1:])}:\n"
+        for i, layer in enumerate(self.network):
+            if i != len(self.network)-1:
+                text += f"Hidden layer {i+1}:\n" 
+            else:
+                text += "Output layer:\n"
             for p in layer:
-                text += str(p) + ' '
-            text += '\n'
+                text += str(p) + " "
+            text += "\n"
         return text
-    
--- models/perceptron.py
+++ models/perceptron.py
@@ -2,32 +2,38 @@ import numpy as np
 
 
 class Perceptron:
-    def __init__(self, inputs=None, weights=None, bias=None, activation_func: str = "sign", learning_const=0.1):
+    def __init__(self, inputs=None, weights=None, bias=None, activation_func: str = "sign", learning_const=0.1,output_layer = False):
 
-        if input is not None and weights is not None and len(inputs) != len(weights):
+        if inputs is not None and weights is not None and len(inputs) != len(weights):
             raise Exception("Vectors must have same size")
 
         self.inputs = np.array(inputs)
         self.weights = np.array(weights)
-        self.bias = bias
+        if bias == None:
+            self.bias = np.random.random()
+        else:
+            self.bias = bias
         self.activation_func = activation_func
         self.learning_const = learning_const
         self.output = None
+        self.is_output_layer = output_layer
         self._activation_fucntion = {
             "sign": self._sign,
             "sigmoid": self._sigmoid,
             "tanh": self._tanh,
             "relu": self._relu,
         }
-        self._activation_fucntion_deriv = {
-            "sign": self._sign_deriv,
-            "sigmoid": self._sigmoid_deriv,
-            "tanh": self._tanh_deriv,
-            "relu": self._relu_deriv,
-        }
+        # self._activation_fucntion_deriv = {
+        #     "sign": self._sign_deriv,
+        #     "sigmoid": self._sigmoid_deriv,
+        #     "tanh": self._tanh_deriv,
+        #     "relu": self._relu_deriv,
+        # }
 
-    def predict(self):
+    def fit(self, y):
         self.output = self.feedforward()
+        # self.gradient_descent(y)
+        return self.output
 
     def feedforward(self):
         total = (self.inputs * self.weights).sum() + self.bias
@@ -39,49 +45,65 @@ class Perceptron:
         prev_delta_weights = np.array([None] * len(self.weights))
 
         while np.not_equal(delta_weight, prev_delta_weights).any():
-            """Debugging prints - uncomment to use
+            # Debugging prints - uncomment to use
             print(f"delta_weight:{delta_weight}, prev_delta_weights:{prev_delta_weights}")
             print(f"weights:{self.weights}, learning_const:{self.learning_const}")
-            print("----")"""
+            print("----")
 
             prev_delta_weights = delta_weight.copy()
-            deriv = self._activation_fucntion_deriv.get(self.activation_func)
-            delta_weight = delta_weight + self.learning_const * self.inputs * deriv(_class)
+            # deriv = self._activation_fucntion_deriv.get(self.activation_func)
+            delta_weight = delta_weight + self.learning_const * self.inputs * self.deriv * (_class - self.output)
             self.weights = self.weights + delta_weight
-            self.predict()
+            self.output = self.feedforward()
 
     def _sign(self, x):
+        self.deriv = 1
         return 1 if x > 0 else 0
 
-    def _sign_deriv(self, _class):
-        return _class - self.output
+    # def _sign_deriv(self, _class):
+    #     return _class - self.output
 
     def _sigmoid(self, x):
-        sig = 1 / (1 + np.exp(-x))
-        return 1 if sig >= 0.5 else 0
+        f = 1 / (1 + np.exp(-x))
+        self.deriv = f * (1 - f)
+        return f
 
-    def _sigmoid_deriv(self, _class):
-        return (_class - self.output) * self.output * (1 - self.output)
+    # def _sigmoid_deriv(self, _class):
+    #     return (_class - self.output) * self.output * (1 - self.output)
 
     def _tanh(self, x):
-        sig = (np.exp(2 * x) - 1) / (np.exp(2 * x) + 1)
-        return 1 if sig >= 0 else 0
+        f = (np.exp(2 * x) - 1) / (np.exp(2 * x) + 1)
+        self.deriv = 1 - (f * f)
+        return f
 
-    def _tanh_deriv(self, _class):
-        return _class - self.output
+    # def _tanh_deriv(self, _class):
+    #     # TODO implement
+    #     pass
 
     def _relu(self, x):
+        self.deriv = 1
         return max(0, x)
 
-    def _relu_deriv(self, _class):
-        return _class - self.output
+    # def _relu_deriv(self, _class):
+    #     # TODO implement
+    #     pass
 
     def set_weights(self, weights):
+        if self.inputs.all() != None:
+            if len(self.inputs) != len(weights):
+                print(f"new weights: {weights}, inputs: {self.inputs}")
+                raise Exception("Vectors must have same size")
         self.weights = np.array(weights)
+        return self
 
     def set_inputs(self, inputs):
+        if self.weights.all() != None:
+            if len(self.weights) != len(inputs):
+                print(f"weights: {self.weights}, new inputs: {inputs}")
+                raise Exception("Vectors must have same size")
         self.inputs = np.array(inputs)
+        return self
 
     def __str__(self):
-        o = self.output if self.output != None else 'na'
-        return f"Perceptron: {o}"
+        o = self.output if self.output != None else "na"
+        return f"Perceptron output {o}, input:{self.inputs}, weights:{self.weights}\n"
