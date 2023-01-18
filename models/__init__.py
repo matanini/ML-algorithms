c4a34dbd36cf6b20b7f429b00827deebd2910bd7
matanini <matanini@gmail.com>
2023-01-18 16:41:57 +0200

    update helper

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from models.gnb import GNB
-from models.knn import KNN
-from models.kmeans import KMeans
-from models.perceptron import Perceptron
+from Models.gnb import GNB
+from Models.knn import KNN
+from Models.kmeans import KMeans
+
